44762bf62158a4d1bf75765cb66e537c9bf36a17
hhvn <ajmarcs@yahoo.com>
2014-10-06 07:04:06 -0400

    Update nmapformat.py

--- nmapformat.py
+++ nmapformat.py
@@ -35,7 +35,7 @@ import re
 #    scan is time-consuming, these processes will take more 
 #    than just a few seconds to complete.  
 #    
-# 6. The resultant file is nmapformat.txt .
+# 6. The resultant file is final.txt .
 #
 # Details:  This program will start nmap for each IP in the 
 # target_addresses.txt file.  The flags for nmap are:
